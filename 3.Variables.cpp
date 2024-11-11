eb476ea749c237c312768af6859dbe19b09bdd7d
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-10 23:11:11 -0600

    Add files via upload

---
+++ 3.Variables.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+   int var1 = 5;
+   int var2 = 2;
+   int var3 = 4; 
+   
+ 	printf("La multiplicacion de %d * %d * %d es = %d\n", var1,var2,var3,var1*var2*var3);
+ 	system("Pause");
+    return 0;
+}
